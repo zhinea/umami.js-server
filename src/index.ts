c642ceeb13d7478d046c145852bdccfcc0060ca6
Zidna Fadla <yuratamma@gmail.com>
2024-04-10 16:10:35 +0700

    update: add 'api/send' route. and  payload validation

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Elysia, {t} from "elysia";
 import {setupApplication} from "./setup.ts";
 import {isbot} from "isbot";
+import {HOSTNAME_REGEX, IP_REGEX} from "./libs/constants.ts";
 
 
 async function main(){
@@ -11,19 +12,54 @@ async function main(){
     app.get('/', () => "Hello World!")
 
 
-    app.post('/api/send', ({headers}) => {
+    app.post('/api/send', ({headers, body}) => {
 
         if (!process.env.DISABLE_BOT_CHECK && isbot(headers['user-agent'])) {
             return "nope";
         }
 
 
-
-
+        return body;
 
     }, {
         body: t.Object({
-            
+            events: t.Array(
+                t.Object({
+                    payload: t.Object({
+                        data: t.Any(),
+                        hostname: t.String({
+                            pattern: HOSTNAME_REGEX.source
+                        }),
+                        ip: t.String({
+                            pattern: IP_REGEX.source
+                        }),
+                        language: t.String({
+                            maxLength: 35
+                        }),
+                        referrer: t.String(),
+                        screen: t.String({
+                            maxLength: 11
+                        }),
+                        title: t.String(),
+                        url: t.String(),
+                        website: t.String(),
+                        name: t.String({
+                            maxLength: 50
+                        }),
+                        tag: t.MaybeEmpty(t.String({
+                            maxLength: 50
+                        })),
+                        t: t.MaybeEmpty(t.String())
+                    }),
+                    type: t.String({
+                        maxLength: 50,
+                        pattern: /event|identify/i.source
+                    })
+                }),
+                {
+                    maxItems: 50
+                }
+            )
         })
     });
 
